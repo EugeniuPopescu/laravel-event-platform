cd66bce178b835553ff560c929b3bac3f523bbee
Eugen <epopescuu@yahoo.it>
2024-02-05 21:24:58 +0100

    feat(event): magration & seeder

---
+++ config/event.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    [
+        'name' => 'Fiesta Friday',
+        'date' => '2024-02-10',
+        'available_tickets' => 150
+    ],
+    [
+        'name' => 'Saturday Night Dance',
+        'date' => '2024-02-18',
+        'available_tickets' => 100
+    ],
+    [
+        'name' => 'Midnight Madness',
+        'date' => '2024-03-02',
+        'available_tickets' => 120
+    ],
+    [
+        'name' => 'Electric Carnival',
+        'date' => '2024-03-15',
+        'available_tickets' => 200
+    ],
+    [
+        'name' => 'Neon Nights',
+        'date' => '2024-04-05',
+        'available_tickets' => 80
+    ],
+    [
+        'name' => 'Glow Party',
+        'date' => '2024-04-20',
+        'available_tickets' => 180
+    ],
+    [
+        'name' => 'Summer Splash',
+        'date' => '2024-05-10',
+        'available_tickets' => 250
+    ],
+
+];
--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $array_event = config("event");
+
+        foreach ($array_event as $event) {
+            $new_event = new Event();
+            $new_event->fill($event);
+            $new_event->save();
+        }
     }
 }
