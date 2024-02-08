771cacc378d67a0bc5431cb1a269af92aa2cecc3
Eugen <epopescuu@yahoo.it>
2024-02-08 11:35:40 +0100

    feat(show): events

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -17,4 +17,64 @@ class EventController extends Controller
             "payload" => $events
         ]);
     }
+
+    // metodo show che usa il success true/false
+    public function show($id)
+    {
+        $events = Event::with("user")->find($id);
+
+        // STESSO LAVORO, MA CON DIVERSI MODI
+
+        // if ($events) {
+        //     return response()->json([
+        //         "success" => true,
+        //         "payload" => $events
+        //     ]);
+        // } else {
+        //     return response()->json([
+        //         "success" => false,
+        //         "playload" => "Nessun evento corrispondente all'id"
+        //     ]);
+        // }
+
+
+        // return $events ? response()->json([
+        //     "success" => true,
+        //     "payload" => $events
+        // ]) : response()->json([
+        //     "success" => false,
+        //     "playload" => "Nessun evento corrispondente all'id"
+        // ]);
+
+        return response()->json([
+            "success" => $events ? true : false,
+            "payload" => $events ? $events : "Nessun evento corrispondente all'id"
+        ]);
+    }
+
+    // metodo show che usa l'Http code (404 not found) con findOrFail
+    // public function show($id)
+    // {
+    //     $events = Event::with("user")->findOrFail($id);
+
+    //     return response()->json([
+    //         "success" => $events ? true : false,
+    //         "payload" => $events ? $events : "Nessun evento corrispondente all'id"
+    //     ]);
+    // }
+
+    // metodo show che usa l'errore manuale 
+    // public function show($id)
+    // {
+    //     $events = Event::with("user")->find($id);
+
+    //     if (!$events) {
+    //         return response(null, 404);
+    //     }
+
+    //     return response()->json([
+    //         "success" => true,
+    //         "payload" => $events
+    //     ]);
+    // }
 }
